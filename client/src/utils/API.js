697f7dd76a55cb72bc4c92768be52c4700bb5fc6
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 03:23:39 -0400

    just adding comments to client side files.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,57 +1,60 @@
-// route to get logged in user's info (needs the token)
+// Function to get the logged-in user's information from the server (requires a token)
 export const getMe = (token) => {
   return fetch('/api/users/me', {
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
+      authorization: `Bearer ${token}`, // Include the token in the headers for authentication
     },
   });
 };
 
+// Function to create a new user by sending user data to the server
 export const createUser = (userData) => {
   return fetch('/api/users', {
-    method: 'POST',
+    method: 'POST', // Use POST method for creating a new user
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(userData),
+    body: JSON.stringify(userData), // Send user data as JSON in the request body
   });
 };
 
+// Function to log in a user by sending login credentials to the server
 export const loginUser = (userData) => {
   return fetch('/api/users/login', {
-    method: 'POST',
+    method: 'POST', // Use POST method for user login
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(userData),
+    body: JSON.stringify(userData), // Send login credentials as JSON in the request body
   });
 };
 
-// save book data for a logged in user
+// Function to save book data for a logged-in user
 export const saveBook = (bookData, token) => {
   return fetch('/api/users', {
-    method: 'PUT',
+    method: 'PUT', // Use PUT method for updating user data with saved books
     headers: {
       'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
+      authorization: `Bearer ${token}`, // Include the token in the headers for authentication
     },
-    body: JSON.stringify(bookData),
+    body: JSON.stringify(bookData), // Send book data as JSON in the request body
   });
 };
 
-// remove saved book data for a logged in user
+// Function to remove saved book data for a logged-in user
 export const deleteBook = (bookId, token) => {
   return fetch(`/api/users/books/${bookId}`, {
-    method: 'DELETE',
+    method: 'DELETE', // Use DELETE method to remove a saved book
     headers: {
-      authorization: `Bearer ${token}`,
+      authorization: `Bearer ${token}`, // Include the token in the headers for authentication
     },
   });
 };
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+// Function to make a search request to the Google Books API
+// The 'query' parameter is used to specify the search query
 export const searchGoogleBooks = (query) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
