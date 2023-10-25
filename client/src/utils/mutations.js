cd52b5bf399007497722caf69960a270cc57db93
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 01:38:15 -0400

    added to mutations in client side, the ADD_USER which will create new profiles taking in username email and password

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,3 +25,29 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+//This mutation is intended to create a new user. It expects the following variables: username, email, and password. 
+//When executed, it returns a token and the user's ID, username, and email.
+// tip to DEBUG: verify that the server-side mutation (addUser) and corresponding resolver are correctly defined and that front-end code properly uses this mutation to create a new user.
+
+export const ADD_USER = gql `
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token     // The mutation should return a token
+        user {     // The mutation should also return user data
+          _id      // The user's ID
+          username // The user's username
+          email    // The user's email
+          bookCount
+            savedBooks {
+              bookId
+              authors
+              description
+              title
+              image
+              link
+        }
+      }
+    }
+  }
+`;
