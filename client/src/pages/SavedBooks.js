4de188fe10b38d9017552156be30cf67c2616aa1
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 02:06:45 -0400

    added comments to the SavedBooks page. Used the useMutation() Hook to execute the  mutation in the handleDeleteBook(). removed deleteBook() function. removeBookId() is in place

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -53,40 +53,36 @@ const SavedBooks = () => {
   return <h2>LOADING...</h2>;
 }
 
-return (
-  <>
-    <div className="text-light bg-dark p-5">
-      <Container>
-        <h1>Viewing saved books!</h1>
-      </Container>
-    </div>
-          <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
-        <Row>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    </>
+const SavedBooks = ({ userData, handleDeleteBook }) => {
+  return (
+    <Container>
+      {/* Display the title with the number of saved books (if any) */}
+      <h2 className='pt-5'>
+        {userData.me.savedBooks.length
+          ? `Viewing ${userData.me.savedBooks.length} saved ${userData.me.savedBooks.length === 1 ? 'book' : 'books'}:`
+          : 'You have no saved books!'}
+      </h2>
+      <Row>
+        {userData.me.savedBooks.map((book) => (
+          // Create a card for each saved book
+          <Col md="4" key={book.bookId}>
+            <Card border='dark'>
+              {book.image && <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />}
+              <Card.Body>
+                <Card.Title>{book.title}</Card.Title>
+                <p className='small'>Authors: {book.authors}</p>
+                <Card.Text>{book.description}</Card.Text>
+                {/* Add a button to delete the book */}
+                <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  Delete this Book!
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+    // Close the Container and component
   );
 };
 
