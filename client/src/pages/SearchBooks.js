59021ecb7bc1e5fdf127457b7aacc2197e32d595
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 02:16:40 -0400

    added the saveBook use mutation.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -15,14 +15,18 @@ import {
   Row
 } from 'react-bootstrap';
 
-const SearchBooks = () => {
-  // create state for holding returned google api data
-  const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
-  const [searchInput, setSearchInput] = useState('');
+  const SearchBooks = () => {
+    // Create state for holding returned Google API data
+    const [searchedBooks, setSearchedBooks] = useState([]);
+    // Create state for holding our search field data
+    const [searchInput, setSearchInput] = useState('');
+  
+    // Create state to hold saved bookId values
+    const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // create state to hold saved bookId values
-  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+    // Define the useMutation hook for saving a book
+  const [saveBook] = useMutation(SAVE_BOOK);
+
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
