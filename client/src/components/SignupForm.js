3b94bf99cb4a650c04a771cfb2b64888f6c503f2
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 02:53:41 -0400

    added comments in signupform

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,7 +3,6 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client'; // Import useMutation from Apollo Client to execute GraphQL mutations
 // Import the ADD_USER mutation functionality
 import { ADD_USER } from '../utils/mutations';
-import {addUser} from '../utils/API'
 import Auth from '../utils/auth';
 
 // Define your functional component
@@ -26,6 +25,7 @@ const SignupForm = () => {
     setUserFormData({ ...userFormData, [name]: value });
   };
 
+  // Create a function to handle form submission
  const handleFormSubmit = async (event) => {
     event.preventDefault();
     
@@ -36,3 +36,80 @@ const SignupForm = () => {
       event.stopPropagation();
     }
         setValidated(true); // Update validation status
+        try {
+          const { data } = await addUser({
+            variables: { ...userFormData }
+          });
+    
+          Auth.login(data.addUser.token); // Log in the user with the received token
+        } catch (err) {
+          console.error(err);
+          setShowAlert(true);
+        }
+    
+        setUserFormData({
+          username: '',
+          email: '',
+          password: '',
+        });
+      };
+    
+      return (
+        <>
+          {/* This is needed for the validation functionality above */}
+          <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+            {/* Show an alert if the server response is bad */}
+            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+              Something went wrong with your signup!
+            </Alert>
+    
+            <Form.Group className='mb-3'>
+              <Form.Label htmlFor='username'>Username</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Your username'
+                name='username'
+                onChange={handleInputChange}
+                value={userFormData.username}
+                required
+              />
+              <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
+            </Form.Group>
+    
+            <Form.Group className='mb-3'>
+              <Form.Label htmlFor='email'>Email</Form.Label>
+              <Form.Control
+                type='email'
+                placeholder='Your email address'
+                name='email'
+                onChange={handleInputChange}
+                value={userFormData.email}
+                required
+              />
+              <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+            </Form.Group>
+    
+            <Form.Group className='mb-3'>
+              <Form.Label htmlFor='password'>Password</Form.Label>
+              <Form.Control
+                type='password'
+                placeholder='Your password'
+                name='password'
+                onChange={handleInputChange}
+                value={userFormData.password}
+                required
+              />
+              <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+            </Form.Group>
+            <Button
+              disabled={!(userFormData.username && userFormData.email && userFormData.password)}
+              type='submit'
+              variant='success'>
+              Submit
+            </Button>
+          </Form>
+        </>
+      );
+    };
+    
+export default SignupForm;
