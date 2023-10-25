3ab8dba22866acde8c399f2f6f6ca15715c7a440
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 09:16:42 -0400

    added query Update the user's savedBooks array Write the updated user data

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -39,6 +39,19 @@ update: (cache) => {
   // Extract the user's data and their savedBooks array from the cache
   const userDataCache = data.me;
   const savedBooksCache = userDataCache.savedBooks;
+  const updatedBookCache = savedBooksCache.filter((book) => book.bookId !== bookId);
+// Create an updated book cache by filtering out the book with the specified bookId from the savedBooks.
+
+data.me.savedBooks = updatedBookCache;
+// Update the user's savedBooks array in the Apollo Client's cache with the filtered cache.
+
+cache.writeQuery({ query: GET_ME, data: { data: { ...data.me.savedBooks } } });
+// Write the updated user data (with the modified savedBooks array) back to the Apollo Client's cache using the GET_ME query.
+
+// Upon success, remove the book's id from localStorage
+removeBookId(bookId);
+// Additionally, remove the book's id from local storage to keep it in sync with the Apollo Client's cache.
+
 
 }
 
