9a68fd3520b2acaa687545a7971ada67125b5af8
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 00:57:25 -0400

    adding authmiddleware import, apolloserver, typeDefs and resolvers to the server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
+//const routes = require('./routes');
+
+
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+
+// Import required modules and dependencies
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 
+const db = require('./config/connection'); // database connection setup
+
+const {authMiddleware} = require('./utils/auth'); // importing the middleware 
+
+const { typeDefs, resolvers } = require('./schemas'); // Import your GraphQL schema type definition and resolvers
+
+// Create an Express.js application instance
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Define the port on which the server should listen, using the provided PORT or defaulting to 3001
+const PORT = process.env.PORT || 3001;
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
