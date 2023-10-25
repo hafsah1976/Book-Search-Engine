4ce23da1ddebf2340e268f8ec296d438157cca60
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 03:19:04 -0400

    added comments to the localStorage file

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,4 +1,6 @@
+// Function to get saved book IDs from local storage
 export const getSavedBookIds = () => {
+  // Check if there are saved book IDs in local storage, and parse the data
   const savedBookIds = localStorage.getItem('saved_books')
     ? JSON.parse(localStorage.getItem('saved_books'))
     : [];
@@ -6,25 +8,32 @@ export const getSavedBookIds = () => {
   return savedBookIds;
 };
 
+// Function to save book IDs to local storage
 export const saveBookIds = (bookIdArr) => {
   if (bookIdArr.length) {
+    // If there are book IDs in the array, save them to local storage
     localStorage.setItem('saved_books', JSON.stringify(bookIdArr));
   } else {
+    // If the array is empty, remove the saved_books entry from local storage
     localStorage.removeItem('saved_books');
   }
 };
 
+// Function to remove a book ID from local storage
 export const removeBookId = (bookId) => {
   const savedBookIds = localStorage.getItem('saved_books')
     ? JSON.parse(localStorage.getItem('saved_books'))
     : null;
 
   if (!savedBookIds) {
-    return false;
+    return false; // Return false if there are no saved book IDs
   }
 
+  // Filter the book IDs to remove the specified book ID
   const updatedSavedBookIds = savedBookIds?.filter((savedBookId) => savedBookId !== bookId);
+
+  // Update the local storage with the new book IDs
   localStorage.setItem('saved_books', JSON.stringify(updatedSavedBookIds));
 
-  return true;
+  return true; // Return true to indicate success
 };
