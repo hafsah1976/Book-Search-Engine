783c6ca451042c8bdee324773af7590f00b2309b
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 01:13:45 -0400

    added the Me query that checks if user is authenticated using the context-api

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -7,3 +7,18 @@ const { User } = require("../models");
 //function that signs JSON Web Tokens (JWTs), which are used for user authentication and authorization in your GraphQL server.
 const { signToken } = require("../utils/auth");
 
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        // Check if the user is authenticated (context.user will be set if authenticated)
+        if (context.user) {
+          // Retrieve user data from the database, excluding sensitive fields
+          const userData = await User.findOne({ _id: context.user._id }).select(
+            "-__v -password"
+          );
+          return userData; // Return user data
+        }
+        // If not authenticated, throw an AuthenticationError
+        throw new AuthenticationError("You must be logged in!");
+      },
+    },
