1c82a32faf0c41ba59a0fe78528002b9ed953cae
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-25 01:23:32 -0400

    added the gql template literal from apollo server express. added type Query and type Mutation{login}

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server-express');
+
+// Importing the gql template literal function from the Apollo Server Express package.
+  // This is where I define my GraphQL schema using the gql template literal function.
+  // Each type definition corresponds to a part of the GraphQL schema, such as Query, Mutation, User, and Book.
+  // Within these types, I have defined the fields and their types.
+  // For the saveBook mutation, it expects an input type BookInput (with a capital 'B').
+
+const typeDefs = gql`
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
