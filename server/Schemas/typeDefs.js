9875918c9ce292a4f4b5c5bcbc92ed5f31ccc4c7
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-28 15:49:57 -0400

    debugged typeDefs, had defined the saveBook mutations (book: bookInput) instead of (input: bookInput), might have caused errors.

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -3,18 +3,31 @@ const { gql } = require("apollo-server-express"); // Importing the gql template 
 // This is where I define my GraphQL schema using the gql template literal function.
 // Each type definition corresponds to a part of the GraphQL schema, such as Query, Mutation, User, and Book.
 // Within these types, I have defined the fields and their types.
-// For the saveBook mutation, it expects an input type BookInput (with a capital 'B').
 
 const typeDefs = gql`
+
+  type Query {
+      me: User
+  }
+
   type Book {
-    _id: ID
-    authors: [String]!
-    description: String!
     bookId: String!
+    authors: String
+    description: String
+    title: String!
     image: String
     link: String
-    title: String!
   }
+
+  input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
   type User {
     _id: ID
     username: String
@@ -22,26 +35,16 @@ const typeDefs = gql`
     bookCount: Int
     savedBooks: [Book]
   }
-  type Query {
-      me: User
-  }
   type Auth {
   token: ID!
   user: User
   }
-  input BookInput {
-      authors: [String]
-      description: String
-      bookId: String
-      image: String
-      link: String
-      title: String
-  }
+
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(book: BookInput): User
-    removeBook(bookId: String!): User
+    saveBook(input: bookInput): User
+    removeBook(bookId: ID!): User
 }
 `;
 
