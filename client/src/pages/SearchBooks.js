25ffba46b9020b337a89af7c18bcaacf430e22ab
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-28 06:21:49 -0400

    debugging searchbooks:added jumbotron in imports and deleted query import

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {
+  Jumbotron,
   Container,
   Col,
   Form,
   Button,
   Card,
   CardColumns,
-  Row,
 } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
-import { GET_ME } from '../utils/queries';
+//import { GET_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
