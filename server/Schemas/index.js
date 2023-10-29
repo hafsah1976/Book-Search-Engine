611e3d96c82dd247019bf81ea838ec1789cc7b89
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-29 12:34:58 -0400

    updated index.js to export gql as well.

--- server/Schemas/index.js
+++ server/Schemas/index.js
@@ -1,4 +1,9 @@
+const { gql } = require('apollo-server-express');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
-module.exports = { typeDefs, resolvers }
+module.exports = {
+  typeDefs,
+  resolvers,
+  gql,
+};
