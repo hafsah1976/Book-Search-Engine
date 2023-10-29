4b9712758d6f554419f01ef001177989c6c94517
Hafsah Nasreen <hafsah999@gmail.com>
2023-10-29 12:18:31 -0400

    updated me query in resolvers to findbyId and populate savedbooks

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,72 +1,95 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
+const { User, bookSchema } = require("../models");
 const { signToken } = require("../utils/auth");
 
-const resolvers = {
-  Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
-        return userData;
-      }
-      throw new AuthenticationError("You must be logged in!");
-    }
-  },
+  const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        // Check if the user is authenticated (context.user will be set if authenticated)
+        if (context.user) {
+          try {
+            //find user by id 
+            const user = await User.findById(context.user._id).populate("savedBooks");
+            return user;
+          } catch (error) {
+             // If there's an error during data retrieval, throw an AuthenticationError
+            throw new AuthenticationError("Failed to fetch user");
+          }
+        }
+              // If not authenticated, throw an AuthenticationError
+       throw new AuthenticationError("You must be logged in!");
+      },
+    },
+  
 
   Mutation: {
     addUser: async (parent, args) => {
+      // Create a new user in the database
       const user = await User.create(args);
-      const token = signToken(user);
-      return { token, user };
+      const token = signToken(user); // Sign a JWT token for the new user
+      return { token, user }; // Return the token and user data
     },
 
     login: async (parent, { email, password }) => {
+      // Find the user by their email
       const user = await User.findOne({ email });
 
       if (!user) {
+        // If the user is not found, throw an AuthenticationError
         throw new AuthenticationError("No user found with this email. Please create an account to continue.");
       }
 
+      // Check if the provided password is correct
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
+        // If the password is incorrect, throw an AuthenticationError
         throw new AuthenticationError("Incorrect email or password!");
       }
 
-      const token = signToken(user);
-      return { token, user };
+      const token = signToken(user); // Sign a JWT token for the authenticated user
+      return { token, user }; // Return the JWT token and user data
     },
 
     saveBook: async (parent, { input }, { user }) => {
       if (user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: user._id },
-          { $addToSet: { savedBooks: input } },
-          { new: true, runValidators: true }
-        );
-
-        return updatedUser;
+        try {
+          // Create a new book instance and add it to the user's savedBooks array
+          const book = new SaveBookInput(input);
+          const updatedUser = await User.findByIdAndUpdate(
+            user._id,
+            { $addToSet: { savedBooks: book } }, // Add the new book to the savedBooks array
+            { new: true } // Return the updated user data
+          ).populate("savedBooks"); // Populate the 'savedBooks' field for a full response
+          return updatedUser;
+        } catch (error) {
+          // Handle errors when saving the book
+          throw new AuthenticationError("Failed to save book");
+        }
       }
-
+      // If not authenticated, throw an AuthenticationError
       throw new AuthenticationError("You need to be logged in to save a book!");
     },
 
     removeBook: async (parent, { bookId }, { user }) => {
       if (user) {
-        try{ 
+        try {
+          // Remove a book from the user's savedBooks array by bookId
           const updatedUser = await User.findByIdAndUpdate(
             user._id,
             { $pull: { savedBooks: { bookId: bookId } } },
             { new: true }
-          ).populate("savedBooks");
+          ).populate("savedBooks"); // Populate the 'savedBooks' field for a full response
           return updatedUser;
         } catch (error) {
+          // Handle errors when removing the book
           throw new AuthenticationError("Failed to remove book");
         }
       }
+      // If not authenticated, throw an AuthenticationError
       throw new AuthenticationError("Login required to delete a book!");
     },
   }
-  };
+};
 
 module.exports = resolvers;
