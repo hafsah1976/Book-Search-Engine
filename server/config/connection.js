c798998461990865879a78f3026c64e49c8cd6f9
Hafsah Nasreen <hafsah999@gmail.com>
2023-11-01 07:43:21 -0400

    updated connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,12 @@
 // Import the dotenv library to load environment variables from a .env file
-require('dotenv').config();
-const urimongodb = process.env.MONGODB_URI;
+// require('dotenv').config();
+// const urimongodb = process.env.MONGODB_URI;
 
 // Import the Mongoose library to interact with the MongoDB database
 const mongoose = require('mongoose');
 
 //Connect to the MongoDB database using the provided URI
-mongoose.connect(urimongodb || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
 
 // Export the Mongoose connection to be used in other parts of the application
 module.exports = mongoose.connection;
-
