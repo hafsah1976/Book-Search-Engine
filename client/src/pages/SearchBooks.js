e0b5f3ac05760823b2fdd59b2dae18e409b3dc6e
Hafsah Nasreen <hafsah999@gmail.com>
2023-11-01 01:15:12 -0400

    updated searchbooks  to debug

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Container, Col, Form, Button, Card, Row } from 'react-bootstrap';
+import { Container, Col, Form, Button, Card } from 'react-bootstrap';
 import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
@@ -15,15 +15,14 @@ const SearchBooks = () => {
 
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());//state for holding saved bookId values
 
-  const [saveBook, { loading }] = useMutation(SAVE_BOOK);//define the savebook function from mutation
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);//define the savebook function from mutation
 
   useEffect(() => {
     // This effect runs when the component unmounts
-    return () => {
+    return () => 
       // Save the updated `savedBookIds` array to `localStorage`
       saveBookIds(savedBookIds);
-    };
-  }); // Empty dependency array to run the effect only on unmount
+    }); // Empty dependency array to run the effect only on unmount
   
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -33,7 +32,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(
+        `https://www.googleapis/books/v1/volumes?q=${searchInput}`
+        );
 
       if (!response.ok) {
         throw new Error('Something went wrong!');
@@ -46,7 +47,7 @@ const SearchBooks = () => {
         authors: book.volumeInfo.authors || ['No author to display'],
         title: book.volumeInfo.title,
         description: book.volumeInfo.description,
-        link: book.volumeInfo.infoLink,
+        //link: book.volumeInfo.infoLink,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
       }));
 
@@ -56,6 +57,8 @@ const SearchBooks = () => {
       console.error(err);
     }
   };
+
+  //function to save books to local storage
 
   const handleSaveBook = async (bookId) => {
 
@@ -68,33 +71,24 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook({
-        variables: { 
-          bookData:  bookToSave 
-        },
+      const {data} = await saveBook({
+        variables: { bookData: {...bookToSave}}, 
       });
-    console.log("book", response);
-
-     // trying to access the result of the mutation, 
-     const savedBook = response.data.saveBook;
-     console.log("Saved book:", savedBook);
+    console.log("book", savedBookIds);
     
     setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-
-
-    if (loading) return 'Saving to your collection';
     } catch (error) {
-      console.error("Internal error");
+      console.error(error);
     }
   };
 
   return (
     <>
-      <div className="text-light bg-dark p-5">
+      <div fluid className="text-light bg-dark p-5">
         <Container>
           <h1>Search for Books!</h1>
           <Form onSubmit={handleFormSubmit}>
-            <Row>
+            <Form.Row>
               <Col xs={12} md={8}>
                 <Form.Control
                   name="searchInput"
@@ -110,24 +104,24 @@ const SearchBooks = () => {
                   Submit Search
                 </Button>
               </Col>
-            </Row>
+            </Form.Row>
           </Form>
         </Container>
       </div>
 
       <Container>
-        <h2 className='pt-5'>
+        <h2>
           {searchedBooks.length
             ? `Viewing ${searchedBooks.length} results:`
             : 'Search for a book to begin'}
         </h2>
-        <Card>
+        <Col>
           {searchedBooks.map((book) => {
             return (
-              <Row key={book.bookId}>
-              <Card  border='dark'>
+              <Card key={book.bookId} border='dark'>
                 {book.image ? (
-                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' 
+                  />
                 ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
@@ -135,20 +129,21 @@ const SearchBooks = () => {
                   <Card.Text>{book.description}</Card.Text>
                   {Auth.loggedIn() && (
                     <Button
-                      disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
+                      disabled={savedBookIds?.some((savedId) => savedId === book.bookId
+                        )}
                       className='btn-block btn-info'
-                      onClick={() => handleSaveBook(book.bookId)}>
-                      {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
-                        ? 'This book has been saved!'
+                      onClick={() => handleSaveBook(book.bookId)}
+                      >
+                      {savedBookIds?.some((savedId) => savedId === book.bookId)
+                        ? 'This book has already been saved!'
                         : 'Save this Book!'}
                     </Button>
                   )}
                 </Card.Body>
               </Card>
-              </Row>
             );
-          })}
-        </Card>
+                      })}
+              </Col>
       </Container>
     </>
   );
